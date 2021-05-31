e4b966d7ea2a13ecd5015717488a9ed786ff0ca9
chiseadrian <chiseadrian@gmail.com>
2021-05-31 19:17:18 +0200

    rutas static heroku

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ dbConnection();
 app.use(cors())
 
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 // Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/routines', require('./routes/routines') );
-app.use('/api/days', require('./routes/days') );
-app.use('/api/notes', require('./routes/notes') );
-app.use('/api/lists', require('./routes/lists') );
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/routines', require('./routes/routines'));
+app.use('/api/days', require('./routes/days'));
+app.use('/api/notes', require('./routes/notes'));
+app.use('/api/lists', require('./routes/lists'));
 
 
 // Escuchar peticiones
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
 
 
